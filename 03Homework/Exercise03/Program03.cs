2d184faa332173e46e784705ffdac944440df0f6
Ljupcho Kalkov <cr2000@users.noreply.github.com>
2020-03-13 22:39:59 +0100

    task 3 changes
    hello

--- 03Homework/Exercise03/Program03.cs
+++ 03Homework/Exercise03/Program03.cs
@@ -1,49 +1,35 @@
 ﻿using System;
 
-namespace ConsoleApp2
+namespace Homework_Class_3
 {
-    class Program03
+    class Program
     {
         static void Main(string[] args)
         {
-
-            //            Create an array with names
-            //Give an option to the user to enter a name from the keyboard
-            //After entering the name put it in the array
-            //Ask the user if they want to enter another name(Y / N)
-            //Do the same process over and over until the user enters N
-            //Print all the names after user enters N
-            //Hint: we need to use endless loop until user enter N, then we want to "break" the loop
-
-
-
-
-
-
-
-
-
-
-
-
-
-            string[] names = new string[5];
-
-            for (int i = 0; i < names.Length; i++)
+            string[] names = new string[10];
+            var index = 0;
+            Console.WriteLine("Please enter a name:");
+            while (true)
             {
-                Console.Write("Please enter a name: ");
-                names[i] = Console.In.ReadLine();
+                string userName = Console.ReadLine();
+                if (userName.Length > 1)
+                {
+                    names[index] = userName;
+                    index++;
+                }
+                Console.WriteLine("Would you like to enter another name? Y/N?");
+                if (userName.ToLower() == "n")
+                {
+                   
+                    Console.WriteLine("\nThe entered names are: ");
+                    foreach (var name in names)
+                    {
+                        Console.Write(name + " ");
+                    }
+                    break;
+                }
             }
-            Console.WriteLine("The entered names are: ");
-            for (int i = 0; i < names.Length; i++)
-            {
-                Console.Write(names[i] + " ");
-            }
-
-
-
             Console.ReadLine();
-
         }
     }
 }
