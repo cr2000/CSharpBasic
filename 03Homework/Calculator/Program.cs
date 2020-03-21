aef22ef7888e36b388d8304520e11cd135b9574c
Ljupcho Kalkov <cr2000@users.noreply.github.com>
2020-03-21 21:11:24 +0100

    change to calculator

--- 03Homework/Calculator/Program.cs
+++ 03Homework/Calculator/Program.cs
@@ -7,7 +7,64 @@ namespace Calculator
         static void Main(string[] args)
         {
             Console.WriteLine("Enter the desired operation +, -, /, * ");
+            string operationChosen = Console.ReadLine();
+            if (operationChosen == "")
+            {
+                Console.WriteLine("Invalid operation selected. The application will automaticaly close.");
+                Console.ReadLine();
+                return;
+            }
             
+            Console.WriteLine("Please Enter a number: ");
+            //double num1 = Convert.ToDouble(Console.ReadLine());
+            double num1;
+            bool isNumeric = double.TryParse(Console.ReadLine(), out num1);
+            if (isNumeric != true)
+            {
+                Console.WriteLine("Invalid number entered. The application will automaticaly close.");
+                Console.ReadLine();
+                return;
+            }
+
+            Console.WriteLine("Please Enter another number: ");
+            //double num2 = Convert.ToDouble(Console.ReadLine());
+            double num2;
+            bool isNumericScnd = double.TryParse(Console.ReadLine(), out num2);
+            if (isNumericScnd != true)
+            {
+                Console.WriteLine("Invalid number entered. The application will automaticaly close.");
+                Console.ReadLine();
+                return;
+            }
+
+
+
+            if (operationChosen == "+")
+            {
+                Console.WriteLine(num1 + num2);
+            }
+            if (operationChosen == "-")
+            {
+                Console.WriteLine(num1 - num2);
+            }
+            if (operationChosen == "/")
+            {
+                if (num2 == 0)
+                {
+                    Console.WriteLine("Division with zero is not possible. The app will automatically close.");
+                }
+                Console.WriteLine(num1 / num2);
+            }
+            if (operationChosen == "*")
+            {
+                Console.WriteLine(num1 * num2);
+            }
+           
+            
+
+
+
+            Console.ReadLine();
         }
     }
 }
