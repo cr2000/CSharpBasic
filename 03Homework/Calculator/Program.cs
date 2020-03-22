2e0e7c144733fb651980a54563f370f10f09c15f
Ljupcho Kalkov <cr2000@users.noreply.github.com>
2020-03-22 04:23:31 +0100

    calc update

--- 03Homework/Calculator/Program.cs
+++ 03Homework/Calculator/Program.cs
@@ -6,34 +6,42 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter the desired operation +, -, /, * ");
-            //if (Console.ReadLine() == "s" || Console.ReadLine() == "S")
-            //{
-            //    Console.ForegroundColor = ConsoleColor.Green;
-            //    Console.WriteLine("Thanks for using the calculator! The aplication will automatically close.");
-            //    Console.ReadLine();
-            //    return;
-            //}
+            while (true)
+            {
 
-            string operationChosen = Console.ReadLine();
-            if (operationChosen == "")
-            {
-                Console.WriteLine("Invalid operation selected. The application will automaticaly close.");
-                Console.ReadLine();
-                return;
-            }
-            
-            Console.WriteLine("Please Enter a number: ");
-            //double num1 = Convert.ToDouble(Console.ReadLine());
-            double num1;
-            bool isNumeric = double.TryParse(Console.ReadLine(), out num1);
-            if (isNumeric != true)
-            {
-                Console.WriteLine("Invalid number entered. The application will automaticaly close.");
-                Console.ReadLine();
-                return;
-            }
-            
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.WriteLine("Enter the desired operation +, -, /, * ");
+               
+
+                string operationChosen = Console.ReadLine();
+                if (operationChosen == "")
+                {
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Invalid operation selected. Please try again.");
+                    Console.ReadLine();
+                    continue;
+                }
+
+                if (operationChosen == "s" || operationChosen == "S")
+                {
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Thanks for using the calculator!Please press enter and try again.");
+                    Console.ReadLine();
+                    return;
+                }
+
+                Console.WriteLine("Please Enter a number: ");
+                //double num1 = Convert.ToDouble(Console.ReadLine());
+                double num1;
+                bool isNumeric = double.TryParse(Console.ReadLine(), out num1);
+                if (isNumeric != true)
+                {
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Invalid number entered. Press eneter and try again.");
+                    Console.ReadLine();
+                    continue;
+                }
+
 
 
                 Console.WriteLine("Please Enter another number: ");
@@ -42,39 +50,41 @@ namespace Calculator
                 bool isNumericScnd = double.TryParse(Console.ReadLine(), out num2);
                 if (isNumericScnd != true)
                 {
-                    Console.WriteLine("Invalid number entered. The application will automaticaly close.");
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Invalid number entered. Please press enter nd try again.");
                     Console.ReadLine();
-                    return;
+                    continue;
                 }
-            
 
 
-            if (operationChosen == "+")
-            {
-                Console.WriteLine(num1 + num2);
-            }
-            if (operationChosen == "-")
-            {
-                Console.WriteLine(num1 - num2);
-            }
-            if (operationChosen == "/")
-            {
-                if (num2 == 0)
+
+                if (operationChosen == "+")
                 {
-                    Console.WriteLine("Division with zero is not possible. The app will automatically close.");
+                    Console.WriteLine(num1 + num2);
                 }
-                Console.WriteLine(num1 / num2);
+                if (operationChosen == "-")
+                {
+                    Console.WriteLine(num1 - num2);
+                }
+                if (operationChosen == "/")
+                {
+                    if (num2 == 0)
+                    {
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Division with zero is not possible. Please try again.");
+                    }
+                    Console.WriteLine(num1 / num2);
+                }
+                if (operationChosen == "*")
+                {
+                    Console.WriteLine(num1 * num2);
+                }
+                Console.ReadLine();
+
+
             }
-            if (operationChosen == "*")
-            {
-                Console.WriteLine(num1 * num2);
-            }
-           
-            
 
 
-
-            Console.ReadLine();
         }
     }
 }
