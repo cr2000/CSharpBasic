2a3dd78a439a023c1465b5fed2a7a2ed714dbc32
Ljupcho Kalkov <cr2000@users.noreply.github.com>
2020-03-22 18:42:59 +0100

    LoginRegister Created

---
+++ 03Homework/LoginRegister/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LoginRegister
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
