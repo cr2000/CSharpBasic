1a833e7994191537158238a2de326adf85701cbe
Ljupcho Kalkov <58531807+cr2000@users.noreply.github.com>
2020-03-24 03:08:22 +0100

    update to register

--- 03Homework/LoginRegister/Program.cs
+++ 03Homework/LoginRegister/Program.cs
@@ -6,7 +6,83 @@ namespace LoginRegister
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            while (true)
+            {
+
+
+                Console.WriteLine("Please enter 1 if you want to register, or 2 if you want to login.");
+                string registerOrLogin = Console.ReadLine();
+                //if (registerOrLogin != "1" && registerOrLogin != "2") 
+                //{
+                //    Console.WriteLine("Invalid choise. Pleaase try again!");
+                //    continue;
+
+                //}
+
+
+
+
+
+                switch (registerOrLogin)
+                {
+                    case "1":
+
+
+                        Console.WriteLine("Please enter your username/emal: ");
+                        string username = Console.ReadLine();
+                        bool userCorrect = false;
+                        if (username.ToLower().Contains('@') && username.ToLower().Contains('.') && username.Length < 30)
+                        {
+                            userCorrect = true;
+                        }
+                        if (userCorrect)
+                        {
+                            Console.WriteLine("Please enter a password:");
+                        }
+
+
+                        if (userCorrect == false)
+                        {
+                            Console.WriteLine("You have entered an invalid username, please try again!");
+                        }
+
+                        break;
+
+
+
+                    case "2":
+
+
+                        break;
+
+
+
+
+
+                    default:
+                        Console.WriteLine("Invalid choise. Pleaase try again!");
+                        break;
+                }
+
+
+                Console.ReadLine();
+
+
+
+
+
+
+            }
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
